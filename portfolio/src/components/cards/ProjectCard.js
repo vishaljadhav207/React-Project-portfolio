bf253fd491a9f52257dc7aa8dce1d236a1dbee85
vishal jadhav <vj7708152@gmail.com>
2023-09-22 13:49:51 +0530

    adding files

---
+++ portfolio/src/components/cards/ProjectCard.js
@@ -0,0 +1,119 @@
+import React from "react";
+import styled from "styled-components";
+
+
+const Card = styled.div`
+  width: 330px;
+  height: 490px;
+  background-color: ${({ theme }) => theme.card};
+  cursor: pointer;
+  border-radius: 10px;
+  box-shadow: 0 0 12px 4px rgba(0, 0, 0, 0.4);
+  overflow: hidden;
+  padding: 26px 20px;
+  display: flex;
+  flex-direction: column;
+  gap: 15px;
+  transition: all 0.5s ease-in-out;
+  &:hover {
+    transform: translateY(-10px);
+    box-shadow: 0 0 50px 4px rgba(0, 0, 0, 0.6);
+    filter: brightness(1.1);
+  }
+`;
+const Image = styled.img`
+  width: 100%;
+  height: 180px;
+  background-color: ${({ theme }) => theme.white};
+  border-radius: 10px;
+  box-shadow: 0 0 16px 2px rgba(0, 0, 0, 0.3);
+`;
+
+const Tags = styled.div`
+  width: 100%;
+  display: flex;
+  flex-wrap: wrap;
+  gap: 13px;
+  margin-top: 4px;
+  align-items: center;
+`;
+
+const Details = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: 0px;
+  padding: 0px 2px;
+`;
+
+const Tag = styled.span`
+  font-size: 12px;
+  font-weight: 400;
+  color: ${({ theme }) => theme.primary};
+  background-color: ${({ theme }) => theme.primary + 15};
+  border-radius: 10px;
+`;
+
+const Title = styled.div`
+  font-size: 20px;
+  font-weight: 600;
+  color: ${({ theme }) => theme.text_secondary};
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  max-width: 100%;
+  -webkit-line-clamp: 2;
+  -webkit-box-orient: verticle;
+`;
+
+const Date = styled.div`
+  font-size: 12px;
+  font-weight: 400;
+  margin-left: 2px;
+  color: ${({ theme }) => theme.text_secondary + 80};
+
+  @media (max-width: 768px) {
+    font-size: 10px;
+  }
+`;
+
+const Description = styled.div`
+  font-weight: 400;
+  color: ${({ theme }) => theme.text_secondary + 90};
+  overflow: hidden;
+  text-overflow: ellipsis;
+  -webkit-line-clamp: 3;
+  -webkit-box-orient: vertical;
+  display: -webkit-box;
+  margin-top: 8px;
+  max-width: 100%;
+`;
+
+const ProjectCard = ({ project }) => {
+  return (
+    <Card>
+      <Image src={project.image} />
+      <Tags>
+        {project.tags.map((tag) => (
+          <Tag key={tag}>{tag}</Tag>
+        ))}
+      </Tags>
+      <Details>
+        <Title>{project.title}</Title>
+        <Date>{project.date}</Date>
+        <Description>{project.description}</Description>
+      </Details>
+      <a
+        href={project.webapp}
+        class="button"
+        target="_blank"
+        style={{ color: "white" }}
+      >
+        View Live
+      </a>
+      
+    </Card>
+  );
+};
+
+export default ProjectCard;
---
+++ portfolio/src/components/projects/index.js
@@ -0,0 +1,84 @@
+import React from "react";
+import styled from "styled-components";
+import ProjectCard from "../cards/ProjectCard";
+import { projects } from "../../data/constants";
+
+const Container = styled.div`
+
+position: relative;
+display: flex;
+justify-content: space-between;
+align-items: center;
+flex-direction: column;
+width: 100%;
+max-width: 1350px;
+padding: 10px 0px 100px 0;
+gap: 12px;
+@media (max-width: 960px) {
+    flex-direction: column;
+}
+  
+`;
+
+const Wrapper = styled.div`
+position: relative;
+display: flex;
+justify-content: space-between;
+align-items: center;
+flex-direction: column;
+width: 100%;
+max-width: 1350px;
+padding: 10px 0px 100px 0;
+gap: 12px;
+@media (max-width: 960px) {
+    flex-direction: column;
+}
+`;
+
+export const Title = styled.div`
+  font-size: 42px;
+  text-align: center;
+  font-weight: 600;
+  margin-top: 20px;
+  color: ${({ theme }) => theme.text_primary};
+  @media (max-width: 768px) {
+    margin-top: 12px;
+    font-size: 32px;
+  }
+`;
+
+export const Desc = styled.div`
+  font-size: 18px;
+  text-align: center;
+  max-width: 600px;
+  color: ${({ theme }) => theme.text_secondary};
+  @media (max-width: 768px) {
+    font-size: 16px;
+  }
+`;
+const CardContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-item: center;
+  flex-wrap: wrap;
+  gap: 28px;
+  
+`;
+
+const Projects = () => {
+  return (
+    <Container id="projects">
+      <Wrapper>
+        <Title>Projects</Title>
+        <Desc>"Here are some of the projects I have worked on."</Desc>
+        <CardContainer>
+          {projects.map((project) => (
+            <ProjectCard project={project}/>
+          ))}
+        </CardContainer>
+      </Wrapper>
+    </Container>
+  );
+};
+
+export default Projects;
